2f33c389727e603c5c3cf6823a3193ea8dbde44e
Marcin Filipowski <proartz.devon@gmail.com>
2019-02-01 18:20:08 +0100

    Add login function

---
+++ src/com/proartz/FXMLChallenge/Controller.java
@@ -0,0 +1,14 @@
+package com.proartz.FXMLChallenge;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class Controller {
+    @FXML private Text actionTarget;
+
+
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actionTarget.setText("Sign In Button Pressed");
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module FXMLChallenge {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens com.proartz.FXMLChallenge;
+}
